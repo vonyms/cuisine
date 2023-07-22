7f199d7418df9129083e3f34722a141438e3cfc7
vonyms <vonirandria07@gmail.com>
2023-07-22 17:52:59 +0300

    set footer

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,1 +1,7 @@
-console.log('nav.js');
+if(document.querySelector('.menu')) {
+    document.querySelector('.menu').addEventListener('click', function() {
+        this.classList.toggle('opened');
+        this.setAttribute('aria-expanded', this.classList.contains('opened'));
+        document.querySelector('.nav-mobile').classList.toggle('opened');
+    });
+}
