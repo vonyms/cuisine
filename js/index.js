7488fc1ed06be1a4fb43de343d70da09578ffa48
vonyms <vonirandria07@gmail.com>
2023-07-22 18:23:01 +0300

    Initial static subtree commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+if(document.querySelector('.menu')) {
+    document.querySelector('.menu').addEventListener('click', function() {
+        this.classList.toggle('opened');
+        this.setAttribute('aria-expanded', this.classList.contains('opened'));
+        document.querySelector('.nav-mobile').classList.toggle('opened');
+        document.body.classList.toggle('fixed-scroll');
+    });
+}
